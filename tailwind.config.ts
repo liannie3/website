ece81501ea5e2014fed9dec9382bf3cf0bfd8204
liannie3 <138341881+liannie3@users.noreply.github.com>
2025-01-12 17:52:54 -0600

    base

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        solanel: ['Solanel', 'sans-serif'],
+      },
     },
   },
   plugins: [],
