60b71ebc4522d1039a6466cc1c0279a397ec0d72
liannie3 <138341881+liannie3@users.noreply.github.com>
2025-01-13 22:34:42 -0600

    base

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,48 @@
+"use client";
+import Link from 'next/link';
 import Image from "next/image";
+import { usePathname } from 'next/navigation';
 
 export default function Home() {
+  const pathname = usePathname();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-20 gap-16 sm:p-4 font-solanel">
+      <header className="">
+        <div className="grid grid-cols-10 w-full">
+          <div className="col-start-1 col-span-4 lg:col-span-3 flex items-center">
+            <Image
+              src="/icon.png"
+              alt="icon"
+              width={24}
+              height={24}
+              className="mr-2"
+            />
+            <Link 
+              href="/" passHref
+              className="inline-block a11y-focus">ANNIE LI
+            </Link>
+          </div>
+          <div className="col-start-8 col-span-3">
+            <nav className="ml-auto flex space-x-[8px] md:space-x-[12px] w-full justify-end">
+              <Link 
+                href="/projects" passHref
+                className={`inline-block a11y-focus ${pathname === '/projects' ? 'font-bold' : ''}`}
+              >PROJECTS
+              </Link>
+              <Link 
+                href="/visuals" passHref
+                className={`inline-block a11y-focus ${pathname === '/visuals' ? 'font-bold' : ''}`}
+              >VISUAL ARTS
+              </Link>
+              <Link 
+                href="/about" passHref
+                className={`inline-block a11y-focus ${pathname === '/about' ? 'font-bold' : ''}`}
+              >ABOUT
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -49,6 +89,7 @@ export default function Home() {
           </a>
         </div>
       </main>
+      
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
