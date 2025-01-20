8b93630692dde152c86eb5cc5d0da83006c4afb3
liannie3 <liannie@tamu.edu>
2025-01-20 16:06:07 -0600

    about & home drafts

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ import { usePathname } from 'next/navigation';
 export default function Home() {
   const pathname = usePathname();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-8 gap-16 sm:p-4 font-solanel">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-8 gap-8 px-32 sm:p-4 font-solanel">
       <header className="px-4">
         <div className="grid grid-cols-10 w-full">
           <div className="col-start-1 col-span-4 lg:col-span-3 flex items-center">
@@ -44,55 +44,33 @@ export default function Home() {
           </div>
         </div>
       </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      <main className="flex flex-col gap-0 row-start-2 items-center px-72 sm:items-start">
+        <section className="justify-start">
+          <h1 className="text-8xl">About</h1>
+        </section>
+        <section className="flex flex-col sm:flex-row max-w-full gap-8 sm:gap-12 ml-auto items-center">
+          <div className="flex flex-col gap-4">
+            <p className="">
+            Howdy! I’m Annie Li, an electrical engineering student at Texas A&M University. My current interests are hardware security and machine learning, especially the intersection of the two. I’m also organizing hackathons at <u>TAMUhack</u>, researching at the <u>SETH Lab</u>, and incoming at <u>HPE</u> for summer 2025.
+            </p>
+            <p>In my free time I like drawing, cooking, and visiting cafes. I tend to pursue random ventures that pique my interest--whether it’s baking, embroidery, or learning the guitar. My most recent one was creating this website!
+            
+            </p>
+          </div>
+          <div className="">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/annie.png"
+              alt="annie"
+              width={700}
+              height={1400}
+              className="mr-2"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          </div>
+        </section>
       </main>
       
       <footer className="px-4">
-        <div className="grid grid-cols-10 w-full">
+        <div className="grid grid-cols-10 w-full px-24">
           <div className="col-start-1 col-span-4 lg:col-span-3 flex items-center space-x-1 mb-0">
             <a 
               href="https://www.linkedin.com/in/liannie3"
