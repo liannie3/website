59df0a422ec1d7c11e7926612faece4bff835a75
liannie3 <liannie@tamu.edu>
2025-02-06 20:12:50 -0600

    oops

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import Header from "@/components/Header";
-import Link from 'next/link';
-import Image from "next/image";
 import Footer from "@/components/Footer";
 
 export default function Home() {
--- app/secret/page.tsx
+++ app/secret/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from 'next/link';
 import Image from "next/image";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
