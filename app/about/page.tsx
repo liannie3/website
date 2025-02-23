c6db996f3ee9bc45a2a8b2404ff40448629c609b
liannie3 <liannie@tamu.edu>
2025-02-23 16:44:49 -0600

    spacing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen w-full gap-4">
       <div className="grid grid-rows-[20px_1fr_20px] w-full max-w-[740px] mx-auto items-center justify-items-center min-h-screen p-4 pb-8 gap-8 sm:p-4 font-solanel">
         <Header />
-          <main className="flex flex-col gap-0 row-start-2 items-center sm:items-start w-full">
+          <main className="flex flex-col gap-2 row-start-2 items-center sm:items-start w-full">
             <section className="flex flex-col sm:flex-row max-w-full gap-8 sm:gap-12 items-center">
               <div className="flex flex-col gap-4">
                 <p className="">
