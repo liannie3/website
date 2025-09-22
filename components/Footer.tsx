0ce568433fce0662f66d40c04b03629e6e19b3c7
liannie3 <liannie@tamu.edu>
2025-09-22 17:08:22 -0500

    Add new custom hover icons for footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,22 +10,19 @@ export default function Header() {
               href="https://www.linkedin.com/in/liannie3"
               target="_blank"
               rel="noopener noreferrer"
-              className="group inline-block a11y-focus">LINKEDIN, 
-              <span className="block h-[1px] bg-[#111f5b] w-0 group-hover:w-full transition-all ease-in-out duration-300"></span>
-            </a>
+              className="cursor-linkedin group inline-block a11y-focus">LINKEDIN, 
+              </a>
             <Link 
               href="/Annie_Li_Resume.pdf"
               target="_blank"
-              className="inline-block a11y-focus group">RESUME, 
-              <span className="block h-[1px] bg-[#111f5b] w-0 group-hover:w-full transition-all ease-in-out duration-300"></span>
-            </Link>
+              className="cursor-resume inline-block a11y-focus group">RESUME, 
+              </Link>
             <a 
               href="https://github.com/liannie3"
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-block a11y-focus group">GITHUB
-              <span className="block h-[1px] bg-[#111f5b] w-0 group-hover:w-full transition-all ease-in-out duration-300"></span>
-            </a>
+              className="cursor-github inline-block a11y-focus group">GITHUB
+              </a>
           </div>
           </h3>
           <div className="col-start-1 col-span-4 md:col-start-7 md:col-span-4 ml-auto flex items-center space-x-1 sm:space-x-1 w-full sm:justify-end">
