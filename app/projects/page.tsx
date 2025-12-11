19e232d4004ce30e168b077bb34a5edce4b6f7fa
liannie3 <liannie@tamu.edu>
2025-12-10 19:30:36 -0600

    Add th26 project and reorganize image files

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
                   </Link>
                 </div>
                 <p className="">
-                  A live audio locater and transcription IoT system for HoH individuals. 
+                  Live audio locater and transcription IoT system for HoH individuals. 
                 </p>
               </div>
             </div>
@@ -63,7 +63,7 @@ export default function Home() {
                     rel="noopener noreferrer"
                     className="inline-block a11y-focus">
                 <Image
-                  src="/gallery/projects/mergebg.png"
+                  src="/projects/merge.png"
                   alt="Merge"
                   width={500}
                   height={1400}
@@ -91,7 +91,7 @@ export default function Home() {
                   </Link>
                 </div>
                 <p className="">
-                  An interactive command line interface application designed to simplify and accelerate Git merge conflict resolution. 
+                  Interactive command line interface application designed to simplify and accelerate Git merge conflict resolution. 
                 </p>
               </div>
             </div>
@@ -104,7 +104,7 @@ export default function Home() {
                     rel="noopener noreferrer"
                     className="inline-block a11y-focus">
                 <Image
-                  src="/gallery/projects/dwbg.png"
+                  src="/projects/dw.png"
                   alt="Dream Weaver"
                   width={500}
                   height={1400}
@@ -132,7 +132,7 @@ export default function Home() {
                   </Link>
                 </div>
                 <p className="">
-                  A 2D platformer game where you can grab the objects out of your background, made for <span className="hover-bg">
+                  2D platformer game where you can grab the objects out of your background, made for <span className="hover-bg">
                   <a href="https://itch.io/jam/chillennium-2025" target="_blank" style={{ fontWeight: 500 }}>
                   <>Chillenium 2025↗&#xFE0E;</></a></span>. My first ever game jam project!
                 </p>
@@ -147,7 +147,7 @@ export default function Home() {
                     rel="noopener noreferrer"
                     className="inline-block a11y-focus">
                 <Image
-                  src="/hh25.png"
+                  src="/projects/hh25.png"
                   alt="HowdyHack 2025"
                   width={500}
                   height={1400}
@@ -175,7 +175,48 @@ export default function Home() {
                   </Link>
                 </div>
                 <p className="">
-                  The event website for Texas A&M’s beginner hackathon with a colorful skater twist!
+                  Event website for Texas A&M’s beginner hackathon with a colorful skater twist!
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-start">
+              <div className="">
+                <Link
+                    href="https://hh25.tamuhack.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block a11y-focus">
+                <Image
+                  src="/projects/th26.png"
+                  alt="TAMUhack 2026"
+                  width={500}
+                  height={1400}
+                  className=""
+                />
+                </Link>
+                </div>
+              <div className="flex flex-col w-full">
+                <div className="flex flex-row w-full justify-between">
+                  <h2 className="">
+                    TAMUHACK 2026
+                  </h2>
+                  <Link
+                    href="https://th26.tamuhack.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block a11y-focus">
+                    <Image
+                      src="/globe-icon.svg"
+                      alt="globe icon"
+                      width={26}
+                      height={26}
+                      className="mt-1"
+                    />
+                  </Link>
+                </div>
+                <p className="">
+                  Event website for Texas A&M’s most fruitful hackathon!
                 </p>
               </div>
             </div>
