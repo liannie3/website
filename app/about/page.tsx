716ec26443a2e2e5a41cfb80ec9dd343d8050f8d
liannie3 <liannie003@gmail.com>
2026-03-31 15:48:52 -0500

    Update about and gallery

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,35 +6,53 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-background text-foreground w-full">
-      <div className="flex flex-col w-full max-w-[770px] mx-auto items-center min-h-dvh p-4 gap-8 font-solanel">
+    <div className="w-full bg-background text-foreground">
+      <div className="mx-auto flex min-h-dvh w-full max-w-[770px] flex-col items-center gap-8 p-4 font-solanel">
         <Header />
-          <main className="flex flex-col flex-grow gap-2 justify-center w-full">
-            <section className="flex flex-col sm:flex-row gap-8 sm:gap-12 items-center">
-              <div className="flex flex-col w-auto gap-4">
-                <p className="">
-                  Howdy! I’m Annie and I’m an undergraduate student studying electrical engineering at Texas A&M University. I’m interested in CPU architecture (design & verification) and low-level systems, and I was a hardware intern at <span className="hover-bg">
-                  <a href="https://hpe.com" target="_blank" style={{ fontWeight: 500 }}>
-                  <>HPE↗&#xFE0E;</></a></span> this past summer. I also organize hackathons at <span className="hover-bg">
-                  <a href="https://tamuhack.org" target="_blank" style={{ fontWeight: 500 }}>
-                  <>TAMUhack↗&#xFE0E;</></a></span> and research vulnerabilities on processor designs at the <span className="hover-bg">
-                  <a href="https://seth.engr.tamu.edu/" target="_blank" 
-                  style={{ fontWeight: 500 }}><>SETH Lab↗&#xFE0E;</></a></span>.
-                </p>
-                <p>
-                  Outside of this I love drawing, playing the piano/guitar/any kind of instrument, playing single-player games and trying new coffee shops! 
-                </p>
-              </div>
-              <Image
-                  src="/annie-sticker.png"
-                  alt="annie"
-                  width={200}
-                  height={1000}
-                  loading="eager"
-                  className="h-auto mr-4 transition duration-300 ease hover:scale-105"
-                />
-            </section>
-          </main>
+        <main className="flex w-full flex-grow flex-col justify-center gap-2">
+          <section className="flex flex-col items-center gap-8 sm:flex-row sm:gap-12">
+            <div className="flex w-auto flex-col gap-4">
+              <p className="">
+                Howdy! I’m Annie and I’m a student studying electrical
+                engineering at Texas A&M University. I’m primarily interested in
+                CPU architecture and low-level systems. I also organize
+                hackathons at{" "}
+                <span className="hover-bg">
+                  <a
+                    href="https://tamuhack.org"
+                    target="_blank"
+                    style={{ fontWeight: 500 }}
+                  >
+                    <>TAMUhack↗&#xFE0E;</>
+                  </a>
+                </span>{" "}
+                and research vulnerabilities on processor designs at the{" "}
+                <span className="hover-bg">
+                  <a
+                    href="https://seth.engr.tamu.edu/"
+                    target="_blank"
+                    style={{ fontWeight: 500 }}
+                  >
+                    <>SETH Lab↗&#xFE0E;</>
+                  </a>
+                </span>
+                .
+              </p>
+              <p>
+                Outside of this I love drawing, playing the piano/guitar,
+                playing single-player games and trying new coffee shops!
+              </p>
+            </div>
+            <Image
+              src="/annie-sticker.png"
+              alt="annie"
+              width={200}
+              height={1000}
+              loading="eager"
+              className="ease mr-4 h-auto transition duration-300 hover:scale-105"
+            />
+          </section>
+        </main>
         <Footer />
       </div>
     </div>
--- app/visuals/page.tsx
+++ app/visuals/page.tsx
@@ -1,36 +1,36 @@
 "use client";
 import Image from "next/image";
 import Header from "@/components/Header";
-import Gallery from "@/components/Gallery"
+import Gallery from "@/components/Gallery";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="bg-background text-foreground w-full">
-      <div className="flex flex-col w-full max-w-[770px] mx-auto items-center min-h-dvh p-4 gap-8 font-solanel">
+    <div className="w-full bg-background text-foreground">
+      <div className="mx-auto flex min-h-dvh w-full max-w-[770px] flex-col items-center gap-8 p-4 font-solanel">
         <Header />
-      <main className="flex flex-col flex-grow items-start w-full">
-        <section className="justify-start flex">
-          <h1 className="text-7xl sm:text-8xl">Illustration</h1>
-          <Image
-            src="/fishs.svg"
-            alt="icon"
-            width={70}
-            height={70}
-            className="ml-4 w-[55px] sm:w-[70px]"
-          />
-        </section>
-        <section className="flex flex-col sm:flex-row max-w-full gap-4 sm:gap-12 items-center">
-          <div className="flex flex-col gap-2">
-            <p className="">
-            Illustration, character and life drawings. Under construction!
-            </p>
-            <Gallery />
-          </div>
-        </section>
-      </main>
-      
-      <Footer />
+        <main className="flex w-full flex-grow flex-col items-start">
+          <section className="flex justify-start">
+            <h1 className="text-7xl">Illustration</h1>
+            <Image
+              src="/fishs.svg"
+              alt="icon"
+              width={70}
+              height={70}
+              className="ml-4 w-[55px] sm:w-[70px]"
+            />
+          </section>
+          <section className="flex max-w-full flex-col items-center gap-4 sm:flex-row sm:gap-12">
+            <div className="flex flex-col gap-2">
+              <p className="">
+                Illustration, character and life drawings. Under construction!
+              </p>
+              <Gallery />
+            </div>
+          </section>
+        </main>
+
+        <Footer />
       </div>
     </div>
   );
