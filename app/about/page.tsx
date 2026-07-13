327c979e35ef5a98eec4feca0f476be90b3a96e2
liannie3 <liannie003@gmail.com>
2026-07-13 12:45:04 -0700

    Update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,30 +2,58 @@
 
 import Image from "next/image";
 import Arrow from "@/components/Arrow";
+import Link from "next/link"
+import { usePathname } from "next/navigation";
 
 export default function Home() {
+  const pathname = usePathname();
   return (
     <main className="flex w-full flex-grow flex-col justify-center gap-2">
       <section className="flex flex-col items-center gap-8 sm:flex-row sm:gap-12">
         <div className="flex w-auto flex-col gap-4">
           <p className="">
-            Howdy! I’m Annie and I’m a student studying electrical engineering
-            at Texas A&M University. I’m primarily interested in CPU
-            architecture and low-level systems. I also organize hackathons at{" "}
+            Howdy! I’m Annie and I study electrical engineering
+            at Texas A&M University. I'm primarily interested in CPU architecture 
+            and embedded systems.  </p><p>
+            I currently work on validating SOCs at <span className="hover-bg">
+              <a
+                href="https://apple.com"
+                target="_blank"
+                className="whitespace-nowrap"
+                style={{ fontWeight: 500 }}
+              >
+                <>Apple<Arrow /></>
+              </a>
+            </span>
+            {" "}and design for hackathons at{" "}
             <span className="hover-bg">
               <a
                 href="https://tamuhack.org"
                 target="_blank"
+                className="whitespace-nowrap"
                 style={{ fontWeight: 500 }}
               >
                 <>TAMUhack<Arrow /></>
               </a>
-            </span>{" "}
-            and research vulnerabilities on processor designs at the{" "}
+            </span>
+            
+            . Previously I did hardware bringup at{" "}
+            <span className="hover-bg">
+              <a
+                href="https://hpe.com/"
+                target="_blank"
+                className="whitespace-nowrap"
+                style={{ fontWeight: 500 }}
+              >
+                <>HPE<Arrow /></>
+              </a>
+            </span>
+             {" "} and researched vulnerabilities on processor designs at the{" "}
             <span className="hover-bg">
               <a
                 href="https://seth.engr.tamu.edu/"
                 target="_blank"
+                className="whitespace-nowrap"
                 style={{ fontWeight: 500 }}
               >
                 <>SETH Lab<Arrow /></>
@@ -34,8 +62,8 @@ export default function Home() {
             .
           </p>
           <p>
-            Outside of this I love drawing, playing the piano/guitar, playing
-            single-player games and trying new coffee shops!
+            Outside of this I love drawing, single-player games, playing the piano/guitar
+             and trying new coffee shops!
           </p>
         </div>
         <Image
