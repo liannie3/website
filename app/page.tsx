f6d7558713b692136cad5d6f8241b667275cd905
liannie3 <liannie003@gmail.com>
2026-07-25 19:33:39 -0700

    Replace landing content with FishPond

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-import Image from "next/image";
+import FishPond from "@/components/FishPond";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-2 grow items-center justify-center sm:items-start">
-      <section className="flex flex-col">
-        <div className="">
-          <Image
-            src="/loafer.webp"
-            alt="placeholder cat that sits there and does nothing"
-            width={125}
-            height={100}
-            className="opacity-95"
-            loading="eager"
-          />
-        </div>
-      </section>
+    <main className="relative flex w-full grow">
+      <FishPond />
     </main>
   );
 }
