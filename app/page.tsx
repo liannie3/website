0fd7b90860b10294fb221554867997ddb45da98f
Jashith Raghavendra <Jashith.Raghavendra@aa.com>
2026-07-31 17:40:28 -0500

    remove useless code

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import FishPond from "@/components/FishPond";
-
-export default function Home() {
-  return (
-    <main className="relative flex w-full grow">
-      <FishPond />
-    </main>
-  );
-}
