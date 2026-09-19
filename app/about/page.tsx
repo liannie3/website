49860ddced3aa45806b1293f813d326753c85f6c
liannie3 <liannie003@gmail.com>
2026-09-19 16:53:28 -0500

    Clean up About html

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
 
-
 export default function Home() {
   return (
-    <main className="flex w-full grow flex-col justify-center gap-2">
-      <section className="flex flex-col items-center gap-8 sm:flex-row sm:gap-12">
-        <div className="flex w-auto flex-col gap-4">
-          <p className="">
-            Howdy! I’m Annie and I study electrical engineering
-            at Texas A&M University. I’m primarily interested in CPU architecture
-            and embedded systems.  </p><p>
-            I currently work on validating SOCs at <span className="hover-bg">
+    <main className="flex grow flex-col justify-center gap-2">
+      <div className="flex flex-col-reverse items-center gap-8 sm:flex-row sm:gap-12">
+        <div className="flex flex-col gap-4">
+          <p>
+            Howdy! I’m Annie and I study electrical engineering at Texas A&M
+            University. I’m primarily interested in CPU architecture and
+            embedded systems.{" "}
+          </p>
+          <p>
+            I currently work on validating SOCs at{" "}
+            <span className="hover-bg">
               <a
                 href="https://apple.com"
                 target="_blank"
@@ -18,20 +20,18 @@ export default function Home() {
               >
                 <>Apple</>
               </a>
+            </span>{" "}
+            and design for hackathons at{" "}
+            <span className="hover-bg">
+              <a
+                href="https://tamuhack.org"
+                target="_blank"
+                className="whitespace-nowrap"
+              >
+                <>TAMUhack</>
+              </a>
             </span>
-            {" "}and design for hackathons at{" "}
-              <span className="hover-bg">
-                <a
-                  href="https://tamuhack.org"
-                  target="_blank"
-                  className="whitespace-nowrap"
-                >
-                  <>TAMUhack</>
-                </a>
-              </span>
-              .
-            {" "}
-            Previously I did hardware bringup at{" "}
+            . Previously I did hardware bringup at{" "}
             <span className="hover-bg">
               <a
                 href="https://hpe.com/"
@@ -40,22 +40,22 @@ export default function Home() {
               >
                 <>HPE</>
               </a>
+            </span>{" "}
+            and researched vulnerabilities on processor designs at the{" "}
+            <span className="hover-bg">
+              <a
+                href="https://seth.engr.tamu.edu/"
+                target="_blank"
+                className="whitespace-nowrap"
+              >
+                <>SETH Lab</>
+              </a>
             </span>
-             {" "} and researched vulnerabilities on processor designs at the{" "}
-              <span className="hover-bg">
-                <a
-                  href="https://seth.engr.tamu.edu/"
-                  target="_blank"
-                  className="whitespace-nowrap"
-                >
-                  <>SETH Lab</>
-                </a>
-              </span>
-              .
+            .
           </p>
           <p>
-            Outside of this I love drawing, single-player games, playing the piano/guitar
-             and trying new coffee shops!
+            Outside of this I love drawing, single-player games, playing the
+            piano/guitar and trying new coffee shops!
           </p>
         </div>
         <Image
@@ -66,7 +66,7 @@ export default function Home() {
           loading="eager"
           className="mr-4 h-auto"
         />
-      </section>
+      </div>
     </main>
   );
 }
